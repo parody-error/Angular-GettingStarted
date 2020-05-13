dde9b050f7eea463502ff8619a60d9728abb4ac2
parody-error <armsitad@gmail.com>
2020-05-12 20:45:42 -0600

    Updating to the end of the thirteenth video

--- APM-Start/src/app/app.module.ts
+++ APM-Start/src/app/app.module.ts
@@ -1,42 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
-import { ProductListComponent } from './products/product-list.component';
-import { StarComponent } from './shared/star.component';
-import { ConvertToSpacesPipe } from './shared/convert-to-spaces.pipe';
-import { ConvertToCharacterPipe } from './shared/convert-to-character.pipe';
-import { ProductDetailComponent } from './products/product-detail.component';
-import { ProductDetailGuard } from './products/product-detail.guard';
 import { WelcomeComponent } from './home/welcome.component';
 import { ErrorComponentComponent } from './home/error-component.component';
+import { ProductModule } from './products/product.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductListComponent,
-    StarComponent,
-    ConvertToSpacesPipe,
-    ConvertToCharacterPipe,
-    ProductDetailComponent,
     WelcomeComponent,
     ErrorComponentComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule,
     HttpClientModule,
     RouterModule.forRoot([
-      { path: 'products', component: ProductListComponent },
-      { path: 'products/:id', canActivate: [ProductDetailGuard], component: ProductDetailComponent },
       { path: 'welcome', component: WelcomeComponent },
       { path: 'error', component: ErrorComponentComponent },
       { path: '', redirectTo: 'welcome', pathMatch: 'full' },
       { path: '**', redirectTo: 'error', pathMatch: 'full' }
-    ])
+    ]),
+    ProductModule
   ],
   bootstrap: [AppComponent]
 })
