a080f48f744f3a3b4be3a21c6ac535dd9c5b72dc
parody-error <armsitad@gmail.com>
2020-05-16 20:48:43 -0600

    Updating end-to-end tests to that they succeed

--- APM-Start/e2e/src/app.po.ts
+++ APM-Start/e2e/src/app.po.ts
@@ -6,6 +6,6 @@ export class AppPage {
   }
 
   getTitleText(): Promise<string> {
-    return element(by.css('pm-root h1')).getText() as Promise<string>;
+    return element(by.css('pm-root a')).getText() as Promise<string>;
   }
 }
